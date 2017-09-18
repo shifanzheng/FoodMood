c6156192cade93b2e6174801b87abfec1650edb0
Shifan <Shifan@10.0.0.109>
2017-09-17 21:16:04 -0400

    Package and Class creation

---
+++ src/PersonalAnalysisController/MicroController.java
@@ -0,0 +1,21 @@
+/*
+ */
+package PersonalAnalysisController;
+
+import PersonalAnalysisModel.MicroModel;
+import PersonalAnalysisView.MicroView;
+
+/**
+ *
+ * @author Shifan
+ */
+public class MicroController {
+    private MicroModel model = new MicroModel();
+    private MicroView view = new MicroView();
+    
+    public MicroController(MicroModel model, MicroView view){
+        
+    }
+    
+    
+}

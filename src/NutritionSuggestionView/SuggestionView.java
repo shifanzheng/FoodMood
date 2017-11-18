50663b7159b75c7b6e016f4e4113e9dd3c284304
JasonHitchings <jhitch1995@gmail.com>
2017-11-17 19:58:17 -0500

    Suggestions gui update

--- src/NutritionSuggestionView/SuggestionView.java
+++ src/NutritionSuggestionView/SuggestionView.java
@@ -41,44 +41,20 @@ public class SuggestionView{
         
         VBox layout = new VBox();
         Label label = new Label("Suggestions View");       
-        Label calorieLimit = new Label ("Enter Calorie Limit");
-        Label dietaryConstraints = new Label ("Other Dietary Constraints");
+
+        TextArea instructions = new TextArea("Thank you for using FoodMood! If you clicked on this tab that means you're looking"+
+        "\nfor some suggestions on what to eat. Lucky for you we have a couple suggestions to"+
+        "\nhelp you make up your mind. If you're looking for something to make you happy"+
+        "\nhow about a nice piece of cake, you've earned it after all. Maybe you want some"+
+        "\nenergy after a workout. In that case we suggest eating something with protein"+
+        "\nand carbs to help you get your energy back, so how about a nice sandwich?"+
+        "\nif you want something to help you relax, might I suggest some pepermint tea?");
         
-        calorieLimitText = new TextField();
-        dietaryConstraintsText = new TextField();
-        TextArea instructions = new TextArea("If you have any special dietary needs, or are watching calories check your suggestions \nfor foods to improve your mood"
-                + " that aren't just healthy, but will make you happy too! \nSimply enter a calorie limit, then type anything you can't eat in the \n'other dietary constraints' box"
-                + " seperated by commas, then hit refresh ");
         instructions.setEditable(false);
         instructions.setFocusTraversable(false);
         instructions.setMouseTransparent(true);
         
-        calorieLimitText.setMaxWidth(160);
-        dietaryConstraintsText.setMaxWidth(160);
-        
-        TableView table = new TableView();
-        
-        table.setEditable(true);
- 
-        TableColumn foodCol = new TableColumn("Food");
-        TableColumn caloriesCol = new TableColumn("Calories");
-        TableColumn proteinCol = new TableColumn("Protein");
-        TableColumn carbsCol = new TableColumn("Carbs");
-        TableColumn fatsCol = new TableColumn("Fats");
-        
-        foodCol.setCellValueFactory(new PropertyValueFactory<>("foodName"));
-        caloriesCol.setCellValueFactory(new PropertyValueFactory<>("calories"));
-        proteinCol.setCellValueFactory(new PropertyValueFactory<>("protein"));
-        carbsCol.setCellValueFactory(new PropertyValueFactory<>("carbs"));
-        fatsCol.setCellValueFactory(new PropertyValueFactory<>("fat"));
-        
-        table.getColumns().addAll(foodCol, caloriesCol, proteinCol, carbsCol, fatsCol);
-        
-        Button refresh = new Button("Refresh");
-        
-      
-      
-        layout.getChildren().addAll(label,calorieLimit,calorieLimitText,dietaryConstraints,dietaryConstraintsText,instructions,refresh,table);
+        layout.getChildren().addAll(label,instructions);
         
         root = new BorderPane(layout);
         
